4c2513d2c52c482f83a5937712b7a892cd7996c2
yms <ymslucky@163.com>
2025-06-18 15:44:31 +0800

    feat(spider): 将 GitHub Trending 数据保存到 Appwrite 数据库
    - 添加 Appwrite 相关环境变量配置
    - 更新项目依赖，增加 Appwrite SDK 和其他必要库
    - 修改 trending.py 以提取更多仓库信息，并保存到 Appwrite 数据库

--- functions/spider/github/trending/trending.py
+++ functions/spider/github/trending/trending.py
@@ -1,12 +1,43 @@
+import os
+
 import requests
+from appwrite.id import ID
 from parsel import Selector
+
+from appwrite.client import Client
+from appwrite.services.databases import Databases
+
+client = Client()
+(client
+ .set_endpoint(os.environ['ENDPOINT'])
+ .set_project(os.environ['PROJECT_ID'])
+ .set_key(os.environ['API_KEY'])
+ )
+databases = Databases(client)
 
 
 def extract_repo(html):
     """提取仓库信息"""
     selector = Selector(text=html)
-    repos = selector.xpath('//h2/a/text()').getall()
-    return [repo.strip() for repo in repos]
+    repos = selector.xpath('//article').getall()
+    rows = []
+    for i in range(len(repos)):
+        href = selector.xpath(f'//article[{i + 1}]/h2/a/@href').get().strip()
+        rows.append({
+            'name': href,
+            'language': selector.xpath(f'//article[{i + 1}]/div[2]/span[1]/span[2]/text()').get().strip(),
+            'star': int(selector.xpath(f'//article[{i + 1}]/div[2]/a[1]/text()').get().strip().replace(",", "")),
+            'description': selector.xpath(f'//article[{i + 1}]/p/text()').get().strip(),
+            'link': f'https://github.com{selector.xpath(f'//article[{i + 1}]/h2/a/@href').get().strip()}'
+        })
+    return rows
+
+
+def save_to_appwrite(repos, context):
+    """保存数据至Appwrite"""
+    for row in repos:
+        result = databases.create_document(os.environ['DATABASE_ID'], os.environ['COLLECTION_ID'], ID.unique(), row)
+        context.log(f'[Trending] 保存数据成功: {result}')
 
 
 def main(context):
@@ -22,5 +53,8 @@ def main(context):
     context.log(f'[Trending] 获取HTML成功，长度: {len(ret_html.text)}')
     repos = extract_repo(ret_html.text)
     context.log(f'[Trending] 提取仓库成功，数量: {len(repos)}')
+
+    # 向Appwrite数据库写入 数据
+    save_to_appwrite(repos, context)
 
     return context.res.success(data=repos)
